44a30d1e9d96f62bba25fa084d908eaca934e6f6
Cyrus Krispin <cyruskrispin@gmail.com>
2025-02-22 02:04:05 +0800

    remove start

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,15 @@
-import { useState } from "react";
 import Header from "./components/header/header";
-import StartButton from "./components/start/start";
 import Board from "./components/board/Board";
 import "./index.css";
 
 function App() {
-  const [gameStarted, setGameStarted] = useState(false);
-
-  const startGame = () => {
-    setGameStarted(true);
-  };
 
   return (
     <>
       <Header githubLink="https://github.com/Cyrus-Krispin/gambitron" />
-      {!gameStarted && <StartButton onStart={startGame} />}
-      {gameStarted ? (
-        <div className="board-container">
-          <Board />
-        </div>
-      ) : (
-        <p className="start-game-message">Click "Start Game" to play!</p>
-      )}
+      <div className="board-container">
+        <Board />
+      </div>
     </>
   );
 }
